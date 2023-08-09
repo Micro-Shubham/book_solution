cd35be01f78b27dccffc39df113c703afe5a174a
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-08-09 23:26:34 +0530

    funtion

---
+++ fun_2.c
@@ -0,0 +1,33 @@
+// Pascal triangle
+#include <stdio.h>
+int factorial(int x) // funtion to calculate factorial
+{
+    int fact = 1;
+    for (int i = 1; i <= x; i++)
+    {
+        fact = fact * i;
+    }
+    return fact;
+}
+int combination(int n, int r) // funtion to calculate combinatio
+{
+    int ncr = factorial(n) / (factorial(r) * factorial(n - r));
+    return ncr;
+}
+int main()
+{
+    int n;
+    printf("enter the number:");
+    scanf("%d", &n);
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            int icj = combination(i, j);
+            printf("%d ", icj);
+            // printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
