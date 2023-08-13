022d17bd2b399d503ff05d1411c61eb691c0c177
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-08-13 14:00:36 +0530

    string basic

---
+++ string_1.c
@@ -0,0 +1,9 @@
+// A string is a sequence of characters terminated with a null character \0.
+//  Note:The null character assign in the last by itself.
+#include <stdio.h>
+int main()
+{
+    char a[5] = {'a', 'c', 'y', 'q', 'p'};
+    printf("%c", a[5]);
+    return 0;
+}
