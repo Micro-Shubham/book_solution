2600de19a52a240a547c865d5e832a315e360b0c
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-08-13 14:39:46 +0530

    space occupy

---
+++ string_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a[4] = {3, 4, 6, 1};
+    char arr[4] = {'b', 'n', 'q', 'p'};
+    printf("Address of integer:%p\n", &a[0]); // understanding the space occupy by a integer and and character.
+    printf("Address of integer:%p\n", &a[1]);
+    printf("Address of integer:%p\n", &a[2]);
+    printf("Address of integer:%p\n", &a[3]);
+    printf("Address of char:%p\n", &arr[0]);
+    printf("Address of char:%p\n", &arr[1]);
+    printf("Address of char:%p\n", &arr[2]);
+    printf("Address of char:%p\n", &arr[3]);
+    return 0;
+}
+// Note:The integer takes more space than a character .
+// char takses 1 byte space where integer takes 4 bytes.
