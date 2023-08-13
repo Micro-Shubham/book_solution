b8da2e2f451d9bdd31cbd744b5763e532b16a12a
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-08-13 16:46:31 +0530

    input and output in string

---
+++ string_7.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[40];
+    gets(str); // taking input for string and whole sentence will be considered.
+
+    puts(str); // print the input being entered by the user.
+    return 0;
+}
