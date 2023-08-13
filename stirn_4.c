f686668f41cea5785bf3727836a2821c29ba0fe2
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-08-13 16:20:34 +0530

    preprocessor

---
+++ stirn_4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h> //this preprocessor is use to access the some library of string.
+int main()
+{
+    char str[] = "Let's work hard for our future.";
+    printf("%s", str); //%s operator is use to print the string.Just write the name of string to print it.
+    // Note:%s is only accessable if I use string.h preprocessor.
+
+    return 0;
+}
