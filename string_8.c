d5b15dad2a572b89c5c9ed8a9a35cb9737b8ba7c
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-08-14 14:49:19 +0530

    reverse string

---
+++ string_8.c
@@ -0,0 +1,27 @@
+// Reverse a string
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[50];
+    puts("Enter the string:");
+    gets(str);
+    int k = 0;
+    int size = 0;
+    while (str[k] != '\0')
+    {
+        size++;
+        k++;
+    }
+    for (int i = 0, j = size - 1; i <= j; i++, j--)
+    {
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+
+    puts("the reverse string is:");
+
+    puts(str);
+    return 0;
+}
