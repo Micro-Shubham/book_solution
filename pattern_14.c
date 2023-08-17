f7486afaddd0ff4c5e2cd5a9f609695bc239bc06
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-08-17 11:22:29 +0530

    numb patr

---
+++ pattern_14.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    for (int i = 1; i <= 4; i++)
+    {
+        int k = i;
+        for (int j = 1; j <= 7; j++)
+        {
+            if (j >= 5 - i && j <= 3 + i)
+            {
+                printf("%d", k);
+                if (j < 4)
+
+                    k++;
+
+                else
+                    k--;
+            }
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
